16907b1f4af2a32e89e006d95411045b8b7afc50
Danielle Nozaki Ogawa <danielle.n.ogawa@hotmail.com>
2023-04-15 13:48:14 -0300

    Initial Commit

---
+++ FavoriteMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FavoriteMovie
+//
+//  Created by Danielle Nozaki Ogawa on 2023/04/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
